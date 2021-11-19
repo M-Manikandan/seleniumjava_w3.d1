bafbdd8d5c30741993dd09e09ea8203fc798efe5
M-Manikandan <93439317+M-Manikandan@users.noreply.github.com>
2021-11-19 19:27:13 +0530

    Add files via upload

---
+++ A7_1.java
@@ -0,0 +1,11 @@
+package week3.day1;
+
+public class A7_1 {
+
+	public static void main(String[] args) {
+		String text=new String("Java Exercise");
+		System.out.println(text.indexOf('E'));
+		System.out.println(text.indexOf('s'));
+	}
+
+}
